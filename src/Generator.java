2e1650f3da566f75bba3d310b90b2a0edb24ff41
Slobb <mboal03@gmail.com>
2024-06-09 19:11:04 -0400

    6/9

---
+++ src/Generator.java
@@ -0,0 +1,24 @@
+class Generator {
+    int randomNum;
+    String RandomChar;
+    String password;
+    public int getRandomNum() {
+        return randomNum;
+    }
+
+    public String getRandomChar() {
+        return RandomChar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setRandomNum(int randomNum) {
+        this.randomNum = randomNum;
+    }
+
+    public String toString() {
+        return password;
+    }
+}
---
+++ src/ReadCSV.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class ReadCSVWithScanner {
+    public static void main(String[] args) {
+        ReadCSVWithScanner test = new ReadCSVWithScanner();
+        LinkedList<PWData> pwData = test.getPWFromCSV("./resources/passwords.csv");
+        for (PWData phonebookEntry: pwData) {
+            System.out.println(phonebookEntry);
+        }
+    }
+
+    public LinkedList<PWData> getPWFromCSV(String filePath) {
+        LinkedList<PWData> pwData = new LinkedList<>();
+        String line = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            while ((line = reader.readLine()) != null) {
+                PWData pw = new PWData();
+                String[] data = line.split(" ");
+                pw.setpw(data[0]);
+                pwData.add(pw);
+            }
+            reader.close();
+        } //fixing later
+        catch (IOException e) {
+            System.out.println("File not found");
+        }
+        return pwData;
+    }
+}
