8e693e73ecae4f0660ef897f5b78fcab4537d247
MarBoal <mboal03@gmail.com>
2024-10-17 17:27:34 -0400

    Basic set up for generator

---
+++ src/PWData.java
@@ -0,0 +1,12 @@
+public class PWData {
+        private String password;
+
+        public void setpw(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String toString() {
+            return "Password: " + password;
+        }
+}
--- src/ReadCSVWithScanner.java
+++ src/ReadCSVWithScanner.java
@@ -1,4 +1,4 @@
-/*import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -7,27 +7,29 @@ public class ReadCSVWithScanner {
     public static void main(String[] args) {
         ReadCSVWithScanner test = new ReadCSVWithScanner();
         LinkedList<PWData> pwData = test.getPWFromCSV("./resources/passwords.csv");
-        for (PWData pwEntry: pwData) {
+        for (PWData pwEntry : pwData) {
             System.out.println(pwEntry);
         }
     }
 
     public LinkedList<PWData> getPWFromCSV(String filePath) {
-        LinkedList<PWData> pwData = new LinkedList<>();
-        String line = null;
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        LinkedList<PWData> pwDataList = new LinkedList<>();
+        String line;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             while ((line = reader.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+
                 PWData pw = new PWData();
                 String[] data = line.split(",");
                 pw.setpw(data[0]);
-                pwData.add(pw);
+                pwDataList.add(pw);
             }
-            reader.close();
-        } //fixing later
-        catch (IOException e) {
-            System.out.println("File not found");
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
         }
-        return pwData;
+        return pwDataList;
     }
-} */
+}
