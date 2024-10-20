2eea5e54dc1a752cc27fb5cfe121be3d639067d3
MarBoal <mboal03@gmail.com>
2024-10-19 21:17:03 -0400

    Password Generator without limit (tested 10m passwords generated, does not check for same pw generated twice)

--- src/RandPasswordGen.java
+++ src/RandPasswordGen.java
@@ -1,10 +1,15 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.*;
 
 public class RandPasswordGen {
-    public static void main(String args[]) {
-        String PW = "";
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("./resources/passwords.csv");
+        PrintWriter out = new PrintWriter(file);
         Random rand = new Random();
-        ArrayList<String> PwVar = new ArrayList<String>();
         ArrayList<String> chars = new ArrayList<String>();
         List<String> ChList = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                 "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -16,11 +21,17 @@ public class RandPasswordGen {
         //82, PW min length 15
         chars.addAll(ChList);
         Collections.shuffle(chars);
-        for (int i = 0; i <= 15; i++) {
-            int num = rand.nextInt(82);
-            PwVar.add(String.valueOf(chars.get(num)));
-            PW = PW + PwVar.get(i);
+        System.out.println("How many passwords do you want to generate?");
+        int loop = Integer.parseInt(scanner.nextLine());
+        for (int x = 0; x < loop; x++) {
+            StringBuilder PW = new StringBuilder();
+            for (int i = 0; i <= 15; i++) {
+                int num = rand.nextInt(82);
+                PW.append(ChList.get(num));
+            }
+            String PWSaver = PW.toString();
+            out.println(PWSaver);
         }
-        System.out.println("Password: " + PW);
+        out.close();
     }
 }
